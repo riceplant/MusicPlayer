552dc4a2f2471747e8fe5bd8da06bc34bd68a757
Binh <binhphanthanh1990@yahoo.de>
2019-07-03 16:16:16 +0200

    added design to main.storyboard

---
+++ Music Player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music Player
+//
+//  Created by Binh Phan on 03.07.19.
+//  Copyright © 2019 Binh Phan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
